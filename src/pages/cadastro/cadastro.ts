608a6a99ba0932a2711f25ef6aff78898c643cf9
IanJairo <ijtg.ian@gmail.com>
2018-12-11 11:42:01 -0300

    Erro no Firebase

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,10 +1,19 @@
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 import { TabsPage } from '../tabs/tabs';
+
+export class Dados {
+  public account_nome: String;
+
+  public account_cpf: string;
+
+}
+
 
 @IonicPage()
 @Component({
@@ -12,19 +21,26 @@ import { TabsPage } from '../tabs/tabs';
   templateUrl: 'cadastro.html',
 })
 export class CadastroPage {
-  public account_nome: String;
   public account_email: string;
   public account_password: string;
-  public account_cpf: string;
-
+  
   public mensagem: string;
+
+  lista: AngularFireList <any>;
+
+  dados:any = Dados;
+
+  
   
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     
+       
     public bancoDeDados: AngularFireDatabase,
     public autenticador: AngularFireAuth) {
+      this.lista = this.bancoDeDados.list('/dados');
+      this.dados = new Dados();
       this.mensagem = '';
   }
 
@@ -38,7 +54,12 @@ export class CadastroPage {
     .catch((erro) => {
       this.mensagem = 'Erro ao cadastrar o usuário: ' + erro;
     })
-    this.navCtrl.push(TabsPage);
+   
+    this.lista.push(this.dados) .then(() => {
+      this.dados = new Dados();
+    })
+
+    this.navCtrl.setRoot(TabsPage);
   }
 
 
